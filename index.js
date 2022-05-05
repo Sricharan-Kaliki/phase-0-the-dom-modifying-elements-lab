366cf7adff48bea85283837937eb995f34ba5625
Sricharan_Kaliki <kalikisrichsanreddy@gmail.com>
2022-05-04 21:20:03 -0400

    commit tree 73b375513464c977ad5e261b34048bcf95fb9132

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+function include(filename)
+{
+    var head = document.getElementsByTagName('head')[0];
+
+    var script = document.createElement('script');
+    script.src = filename;
+    script.type = 'text/javascript';
+
+    head.appendChild(script)
+}
+const index=require('./index.html');
+const main=index.getElementById("main");
+main.remove();
